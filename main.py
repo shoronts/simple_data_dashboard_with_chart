054f7867d9a6118ace7d3d1595b75cb2965df3c2
Md Sharif Foysal Shoron <mdshariffoysalshoron@gmail.com>
2021-12-19 04:16:34 +0600

    Dashboard rebuild

--- main.py
+++ main.py
@@ -15,48 +15,64 @@ def home():
 @app.route('/dashboard', methods=['GET','POST'])
 def dashboard():
     if request.method == 'POST':
-        # Recinving Values from AJAX
-        get_data = request.json
-        # print(get_data['alpha'])
-        # print(get_data['average'])
+        print(request.form)
 
         # Making DATA by Pandas
-        data_lists = list("ABCD")
+        data_lists = list("ABCDEFGHI")
         dates = pd.date_range("20100101", periods=365)
-        random_data = pd.DataFrame(np.random.randn(365, 4), index=dates, columns=data_lists)
+        random_data = pd.DataFrame(np.random.randn(365, 9), index=dates, columns=data_lists)
         random_data.index=pd.to_datetime(random_data.index)
-        data_fream=random_data.tail(5)
-
-        # Find the request type
-        try:
-            next_data = get_data['changeData']
-            is_ajax_request = True
-
-        except:
-            next_data = 'A'
-            is_ajax_request = False
+        data_fream=random_data.tail(15)
 
         # Final data for user
-        data_fream_final=pd.DataFrame(data_fream[next_data]) #This coming from ajax
+        data_fream_final=pd.DataFrame(data_fream['A'])
 
-        # Collecting All Values from Final Datas
+        # Collecting All Values from Final Data
         all_values = []
         for val in data_fream_final.values.tolist():
             all_values.append(str(val)[1:-1])
 
-        # Collecting All Dates From Final Datas
+        # Collecting All Dates From Final Data
         all_dates = []
         for dates in data_fream_final.index:
             pd_to_date = dates.to_pydatetime().strftime("%m/%d/%Y, %H:%M:%S")
             all_dates.append(pd_to_date.split(',')[0])
 
-        # Data sending to the user
-        if is_ajax_request:
-            return jsonify({'values':all_values,'dates':all_dates})
-        else:
-            return render_template("analysis.html",lists=data_lists,dates=all_dates,values=all_values)
+        return render_template("analysis.html",lists=data_lists,dates=all_dates,values=all_values)
+
     else:
         return render_template('dashboard.html')
+
+
+@app.route('/api-v0-data', methods=['POST'])
+def api_data():
+    get_data = request.json
+    # print(get_data['alpha'])
+    # print(get_data['average'])
+
+    # Making DATA by Pandas
+    data_lists = list("ABCDEFGHI")
+    dates = pd.date_range("20100101", periods=365)
+    random_data = pd.DataFrame(np.random.randn(365, 9), index=dates, columns=data_lists)
+    random_data.index=pd.to_datetime(random_data.index)
+    data_fream=random_data.tail(15)
+    next_data = get_data['changeData']
+
+    # Final data for user
+    data_fream_final=pd.DataFrame(data_fream[next_data])
+
+    # Collecting All Values from Final Data
+    all_values = []
+    for val in data_fream_final.values.tolist():
+        all_values.append(str(val)[1:-1])
+
+    # Collecting All Dates From Final Data
+    all_dates = []
+    for dates in data_fream_final.index:
+        pd_to_date = dates.to_pydatetime().strftime("%m/%d/%Y, %H:%M:%S")
+        all_dates.append(pd_to_date.split(',')[0])
+    
+    return jsonify({'values':all_values,'dates':all_dates})
 
 
 @app.route('/download-data', methods=['GET','POST'])
